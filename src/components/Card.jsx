e4ca055146195e769ab4d0fc9c1aabd6fe0a74eb
airi-miyamoto <47703050+airi-miyamoto@users.noreply.github.com>
2025-09-18 13:10:13 +0900

    [update]card.js タイポ修正, ogp設定

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Card({ id, image, name}) {
     return (
-        <Link to={`/detail/${id}`} className="group cursor-pointer text-left block drop-shadow-sm dark:drop-shadow-white aspect-[3/4] w-full h-fullrelative">
+        <Link to={`/detail/${id}`} className="group cursor-pointer text-left block drop-shadow-sm dark:drop-shadow-white aspect-[3/4] w-full h-full relative">
             <div className="rounded-md overflow-hidden w-full h-full absolute inset-[0]">
                 <img className="group-hover:scale-105 transition-transform duration-300 object-cover inset-[0] w-full h-full" src={image} alt={name} />
                 <div className="absolute left-0 bottom-0 w-full aspect-[300/150] z-[1] bg-gradient-to-b from-transparent to-[#390607] opacity-[0.7]"></div>
