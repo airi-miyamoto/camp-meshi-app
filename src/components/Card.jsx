6a3cf9183938fb80a033afe49b86e2601fdc2fe2
airi-miyamoto <47703050+airi-miyamoto@users.noreply.github.com>
2025-09-19 06:52:42 +0900

    [update]index.css tailwindリファクタリング

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ function Card({ id, image, name}) {
                 <img className="group-hover:scale-105 transition-transform duration-300 object-cover inset-[0] w-full h-full" src={image} alt={name} />
                 <div className="absolute left-0 bottom-0 w-full aspect-[300/150] z-[1] bg-gradient-to-b from-transparent to-[#390607] opacity-[0.7]"></div>
             </div>
-            <h3 className="pb-[16px] px-[16px] text-white flex items-center gap-[8px] text-lg font-bold absolute bottom-0 left-0 z-[2] font-sans"><PiHashBold className="shrink-0" />{name}</h3>
+            <h3 className="pb-[16px] px-[16px] text-white flex items-center gap-[8px] text-lg font-bold absolute bottom-0 left-0 z-[2]"><PiHashBold className="shrink-0" />{name}</h3>
         </Link>
     )
 }
